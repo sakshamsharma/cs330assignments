8b1b524c639d8390e800b1ed41dd88388dabb5b4
Saksham Sharma <saksham0808@gmail.com>
2016-08-28 19:11:48 +0530

    Fix indents

--- code/threads/synchlist.cc
+++ code/threads/synchlist.cc
@@ -1,15 +1,15 @@
 // synchlist.cc
-//	Routines for synchronized access to a list.
+//  Routines for synchronized access to a list.
 //
-//	Implemented by surrounding the List abstraction
-//	with synchronization routines.
+//  Implemented by surrounding the List abstraction
+//  with synchronization routines.
 //
-// 	Implemented in "monitor"-style -- surround each procedure with a
-// 	lock acquire and release pair, using condition signal and wait for
-// 	synchronization.
+//  Implemented in "monitor"-style -- surround each procedure with a
+//  lock acquire and release pair, using condition signal and wait for
+//  synchronization.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
@@ -17,26 +17,26 @@
 
 //----------------------------------------------------------------------
 // SynchList::SynchList
-//	Allocate and initialize the data structures needed for a 
-//	synchronized list, empty to start with.
-//	Elements can now be added to the list.
+//  Allocate and initialize the data structures needed for a
+//  synchronized list, empty to start with.
+//  Elements can now be added to the list.
 //----------------------------------------------------------------------
 
 SynchList::SynchList()
 {
     list = new List();
-    lock = new Lock("list lock"); 
+    lock = new Lock("list lock");
     listEmpty = new Condition("list empty cond");
 }
 
 //----------------------------------------------------------------------
 // SynchList::~SynchList
-//	De-allocate the data structures created for synchronizing a list. 
+//  De-allocate the data structures created for synchronizing a list.
 //----------------------------------------------------------------------
 
 SynchList::~SynchList()
-{ 
-    delete list; 
+{
+    delete list;
     delete lock;
     delete listEmpty;
 }
@@ -44,27 +44,27 @@ SynchList::~SynchList()
 //----------------------------------------------------------------------
 // SynchList::Append
 //      Append an "item" to the end of the list.  Wake up anyone
-//	waiting for an element to be appended.
+//  waiting for an element to be appended.
 //
-//	"item" is the thing to put on the list, it can be a pointer to 
-//		anything.
+//  "item" is the thing to put on the list, it can be a pointer to
+//      anything.
 //----------------------------------------------------------------------
 
 void
 SynchList::Append(void *item)
 {
-    lock->Acquire();		// enforce mutual exclusive access to the list 
+    lock->Acquire();        // enforce mutual exclusive access to the list
     list->Append(item);
-    listEmpty->Signal(lock);	// wake up a waiter, if any
+    listEmpty->Signal(lock);    // wake up a waiter, if any
     lock->Release();
 }
 
 //----------------------------------------------------------------------
 // SynchList::Remove
 //      Remove an "item" from the beginning of the list.  Wait if
-//	the list is empty.
+//  the list is empty.
 // Returns:
-//	The removed item. 
+//  The removed item.
 //----------------------------------------------------------------------
 
 void *
@@ -72,9 +72,9 @@ SynchList::Remove()
 {
     void *item;
 
-    lock->Acquire();			// enforce mutual exclusion
+    lock->Acquire();            // enforce mutual exclusion
     while (list->IsEmpty())
-	listEmpty->Wait(lock);		// wait until list isn't empty
+    listEmpty->Wait(lock);      // wait until list isn't empty
     item = list->Remove();
     ASSERT(item != NULL);
     lock->Release();
@@ -84,15 +84,15 @@ SynchList::Remove()
 //----------------------------------------------------------------------
 // SynchList::Mapcar
 //      Apply function to every item on the list.  Obey mutual exclusion
-//	constraints.
+//  constraints.
 //
-//	"func" is the procedure to be applied.
+//  "func" is the procedure to be applied.
 //----------------------------------------------------------------------
 
 void
 SynchList::Mapcar(VoidFunctionPtr func)
-{ 
-    lock->Acquire(); 
+{
+    lock->Acquire();
     list->Mapcar(func);
-    lock->Release(); 
+    lock->Release();
 }
--- code/threads/thread.cc
+++ code/threads/thread.cc
@@ -1,17 +1,17 @@
-// thread.cc 
-//	Routines to manage threads.  There are four main operations:
+// thread.cc
+//  Routines to manage threads.  There are four main operations:
 //
-//	ThreadFork -- create a thread to run a procedure concurrently
-//		with the caller (this is done in two steps -- first
-//		allocate the Thread object, then call ThredFork on it)
-//	Finish -- called when the forked procedure finishes, to clean up
-//	YieldCPU -- relinquish control over the CPU to another ready thread
-//	PutThreadToSleep -- relinquish control over the CPU, but thread is now blocked.
-//		In other words, it will not run again, until explicitly 
-//		put back on the ready queue.
+//  ThreadFork -- create a thread to run a procedure concurrently
+//      with the caller (this is done in two steps -- first
+//      allocate the Thread object, then call ThredFork on it)
+//  Finish -- called when the forked procedure finishes, to clean up
+//  YieldCPU -- relinquish control over the CPU to another ready thread
+//  PutThreadToSleep -- relinquish control over the CPU, but thread is now blocked.
+//      In other words, it will not run again, until explicitly
+//      put back on the ready queue.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
@@ -20,16 +20,16 @@
 #include "synch.h"
 #include "system.h"
 
-#define STACK_FENCEPOST 0xdeadbeef	// this is put at the top of the
-					// execution stack, for detecting 
-					// stack overflows
+#define STACK_FENCEPOST 0xdeadbeef  // this is put at the top of the
+                    // execution stack, for detecting
+                    // stack overflows
 
 //----------------------------------------------------------------------
 // NachOSThread::NachOSThread
-// 	Initialize a thread control block, so that we can then call
-//	NachOSThread::ThreadFork.
+//  Initialize a thread control block, so that we can then call
+//  NachOSThread::ThreadFork.
 //
-//	"threadName" is an arbitrary string, useful for debugging.
+//  "threadName" is an arbitrary string, useful for debugging.
 //----------------------------------------------------------------------
 
 NachOSThread::NachOSThread(char* threadName)
@@ -45,10 +45,10 @@ NachOSThread::NachOSThread(char* threadName)
 
 //----------------------------------------------------------------------
 // NachOSThread::~NachOSThread
-// 	De-allocate a thread.
+//  De-allocate a thread.
 //
-// 	NOTE: the current thread *cannot* delete itself directly,
-//	since it is still running on the stack that we need to delete.
+//  NOTE: the current thread *cannot* delete itself directly,
+//  since it is still running on the stack that we need to delete.
 //
 //      NOTE: if this is the main thread, we can't delete the stack
 //      because we didn't allocate it -- we got it automatically
@@ -61,114 +61,114 @@ NachOSThread::~NachOSThread()
 
     ASSERT(this != currentThread);
     if (stack != NULL)
-	DeallocBoundedArray((char *) stack, StackSize * sizeof(int));
+    DeallocBoundedArray((char *) stack, StackSize * sizeof(int));
 }
 
 //----------------------------------------------------------------------
 // NachOSThread::ThreadFork
-// 	Invoke (*func)(arg), allowing caller and callee to execute 
-//	concurrently.
+//  Invoke (*func)(arg), allowing caller and callee to execute
+//  concurrently.
 //
-//	NOTE: although our definition allows only a single integer argument
-//	to be passed to the procedure, it is possible to pass multiple
-//	arguments by making them fields of a structure, and passing a pointer
-//	to the structure as "arg".
+//  NOTE: although our definition allows only a single integer argument
+//  to be passed to the procedure, it is possible to pass multiple
+//  arguments by making them fields of a structure, and passing a pointer
+//  to the structure as "arg".
 //
-// 	Implemented as the following steps:
-//		1. Allocate a stack
-//		2. Initialize the stack so that a call to SWITCH will
-//		cause it to run the procedure
-//		3. Put the thread on the ready queue
-// 	
-//	"func" is the procedure to run concurrently.
-//	"arg" is a single argument to be passed to the procedure.
+//  Implemented as the following steps:
+//      1. Allocate a stack
+//      2. Initialize the stack so that a call to SWITCH will
+//      cause it to run the procedure
+//      3. Put the thread on the ready queue
+//
+//  "func" is the procedure to run concurrently.
+//  "arg" is a single argument to be passed to the procedure.
 //----------------------------------------------------------------------
 
-void 
+void
 NachOSThread::ThreadFork(VoidFunctionPtr func, int arg)
 {
     DEBUG('t', "Forking thread \"%s\" with func = 0x%x, arg = %d\n",
-	  name, (int) func, arg);
-    
+      name, (int) func, arg);
+
     AllocateThreadStack(func, arg);
 
     IntStatus oldLevel = interrupt->SetLevel(IntOff);
-    scheduler->ThreadIsReadyToRun(this);	// ThreadIsReadyToRun assumes that interrupts 
-					// are disabled!
+    scheduler->ThreadIsReadyToRun(this);    // ThreadIsReadyToRun assumes that interrupts
+                    // are disabled!
     (void) interrupt->SetLevel(oldLevel);
-}    
+}
 
 //----------------------------------------------------------------------
 // NachOSThread::CheckOverflow
-// 	Check a thread's stack to see if it has overrun the space
-//	that has been allocated for it.  If we had a smarter compiler,
-//	we wouldn't need to worry about this, but we don't.
+//  Check a thread's stack to see if it has overrun the space
+//  that has been allocated for it.  If we had a smarter compiler,
+//  we wouldn't need to worry about this, but we don't.
 //
-// 	NOTE: Nachos will not catch all stack overflow conditions.
-//	In other words, your program may still crash because of an overflow.
+//  NOTE: Nachos will not catch all stack overflow conditions.
+//  In other words, your program may still crash because of an overflow.
 //
-// 	If you get bizarre results (such as seg faults where there is no code)
-// 	then you *may* need to increase the stack size.  You can avoid stack
-// 	overflows by not putting large data structures on the stack.
-// 	Don't do this: void foo() { int bigArray[10000]; ... }
+//  If you get bizarre results (such as seg faults where there is no code)
+//  then you *may* need to increase the stack size.  You can avoid stack
+//  overflows by not putting large data structures on the stack.
+//  Don't do this: void foo() { int bigArray[10000]; ... }
 //----------------------------------------------------------------------
 
 void
 NachOSThread::CheckOverflow()
 {
     if (stack != NULL)
-#ifdef HOST_SNAKE			// Stacks grow upward on the Snakes
-	ASSERT(stack[StackSize - 1] == STACK_FENCEPOST);
+#ifdef HOST_SNAKE           // Stacks grow upward on the Snakes
+    ASSERT(stack[StackSize - 1] == STACK_FENCEPOST);
 #else
-	ASSERT(*stack == STACK_FENCEPOST);
+    ASSERT(*stack == STACK_FENCEPOST);
 #endif
 }
 
 //----------------------------------------------------------------------
 // NachOSThread::FinishThread
-// 	Called by ThreadRoot when a thread is done executing the 
-//	forked procedure.
+//  Called by ThreadRoot when a thread is done executing the
+//  forked procedure.
 //
-// 	NOTE: we don't immediately de-allocate the thread data structure 
-//	or the execution stack, because we're still running in the thread 
-//	and we're still on the stack!  Instead, we set "threadToBeDestroyed", 
-//	so that NachOSscheduler::Schedule() will call the destructor, once we're
-//	running in the context of a different thread.
+//  NOTE: we don't immediately de-allocate the thread data structure
+//  or the execution stack, because we're still running in the thread
+//  and we're still on the stack!  Instead, we set "threadToBeDestroyed",
+//  so that NachOSscheduler::Schedule() will call the destructor, once we're
+//  running in the context of a different thread.
 //
-// 	NOTE: we disable interrupts, so that we don't get a time slice 
-//	between setting threadToBeDestroyed, and going to sleep.
+//  NOTE: we disable interrupts, so that we don't get a time slice
+//  between setting threadToBeDestroyed, and going to sleep.
 //----------------------------------------------------------------------
 
 //
 void
 NachOSThread::FinishThread ()
 {
-    (void) interrupt->SetLevel(IntOff);		
+    (void) interrupt->SetLevel(IntOff);
     ASSERT(this == currentThread);
-    
+
     DEBUG('t', "Finishing thread \"%s\"\n", getName());
-    
+
     threadToBeDestroyed = currentThread;
-    PutThreadToSleep();					// invokes SWITCH
+    PutThreadToSleep();                 // invokes SWITCH
     // not reached
 }
 
 //----------------------------------------------------------------------
 // NachOSThread::YieldCPU
-// 	Relinquish the CPU if any other thread is ready to run.
-//	If so, put the thread on the end of the ready list, so that
-//	it will eventually be re-scheduled.
+//  Relinquish the CPU if any other thread is ready to run.
+//  If so, put the thread on the end of the ready list, so that
+//  it will eventually be re-scheduled.
 //
-//	NOTE: returns immediately if no other thread on the ready queue.
-//	Otherwise returns when the thread eventually works its way
-//	to the front of the ready list and gets re-scheduled.
+//  NOTE: returns immediately if no other thread on the ready queue.
+//  Otherwise returns when the thread eventually works its way
+//  to the front of the ready list and gets re-scheduled.
 //
-//	NOTE: we disable interrupts, so that looking at the thread
-//	on the front of the ready list, and switching to it, can be done
-//	atomically.  On return, we re-set the interrupt level to its
-//	original state, in case we are called with interrupts disabled. 
+//  NOTE: we disable interrupts, so that looking at the thread
+//  on the front of the ready list, and switching to it, can be done
+//  atomically.  On return, we re-set the interrupt level to its
+//  original state, in case we are called with interrupts disabled.
 //
-// 	Similar to NachOSThread::PutThreadToSleep(), but a little different.
+//  Similar to NachOSThread::PutThreadToSleep(), but a little different.
 //----------------------------------------------------------------------
 
 void
@@ -176,61 +176,61 @@ NachOSThread::YieldCPU ()
 {
     NachOSThread *nextThread;
     IntStatus oldLevel = interrupt->SetLevel(IntOff);
-    
+
     ASSERT(this == currentThread);
-    
+
     DEBUG('t', "Yielding thread \"%s\"\n", getName());
-    
+
     nextThread = scheduler->FindNextThreadToRun();
     if (nextThread != NULL) {
-	scheduler->ThreadIsReadyToRun(this);
-	scheduler->Schedule(nextThread);
+    scheduler->ThreadIsReadyToRun(this);
+    scheduler->Schedule(nextThread);
     }
     (void) interrupt->SetLevel(oldLevel);
 }
 
 //----------------------------------------------------------------------
 // NachOSThread::PutThreadToSleep
-// 	Relinquish the CPU, because the current thread is blocked
-//	waiting on a synchronization variable (Semaphore, Lock, or Condition).
-//	Eventually, some thread will wake this thread up, and put it
-//	back on the ready queue, so that it can be re-scheduled.
+//  Relinquish the CPU, because the current thread is blocked
+//  waiting on a synchronization variable (Semaphore, Lock, or Condition).
+//  Eventually, some thread will wake this thread up, and put it
+//  back on the ready queue, so that it can be re-scheduled.
 //
-//	NOTE: if there are no threads on the ready queue, that means
-//	we have no thread to run.  "Interrupt::Idle" is called
-//	to signify that we should idle the CPU until the next I/O interrupt
-//	occurs (the only thing that could cause a thread to become
-//	ready to run).
+//  NOTE: if there are no threads on the ready queue, that means
+//  we have no thread to run.  "Interrupt::Idle" is called
+//  to signify that we should idle the CPU until the next I/O interrupt
+//  occurs (the only thing that could cause a thread to become
+//  ready to run).
 //
-//	NOTE: we assume interrupts are already disabled, because it
-//	is called from the synchronization routines which must
-//	disable interrupts for atomicity.   We need interrupts off 
-//	so that there can't be a time slice between pulling the first thread
-//	off the ready list, and switching to it.
+//  NOTE: we assume interrupts are already disabled, because it
+//  is called from the synchronization routines which must
+//  disable interrupts for atomicity.   We need interrupts off
+//  so that there can't be a time slice between pulling the first thread
+//  off the ready list, and switching to it.
 //----------------------------------------------------------------------
 void
 NachOSThread::PutThreadToSleep ()
 {
     NachOSThread *nextThread;
-    
+
     ASSERT(this == currentThread);
     ASSERT(interrupt->getLevel() == IntOff);
-    
+
     DEBUG('t', "Sleeping thread \"%s\"\n", getName());
 
     status = BLOCKED;
     while ((nextThread = scheduler->FindNextThreadToRun()) == NULL)
-	interrupt->Idle();	// no one to run, wait for an interrupt
-        
+    interrupt->Idle();  // no one to run, wait for an interrupt
+
     scheduler->Schedule(nextThread); // returns when we've been signalled
 }
 
 //----------------------------------------------------------------------
 // ThreadFinish, InterruptEnable, ThreadPrint
-//	Dummy functions because C++ does not allow a pointer to a member
-//	function.  So in order to do this, we create a dummy C function
-//	(which we can pass a pointer to), that then simply calls the 
-//	member function.
+//  Dummy functions because C++ does not allow a pointer to a member
+//  function.  So in order to do this, we create a dummy C function
+//  (which we can pass a pointer to), that then simply calls the
+//  member function.
 //----------------------------------------------------------------------
 
 static void ThreadFinish()    { currentThread->FinishThread(); }
@@ -239,14 +239,14 @@ void ThreadPrint(int arg){ NachOSThread *t = (NachOSThread *)arg; t->Print(); }
 
 //----------------------------------------------------------------------
 // NachOSThread::AllocateThreadStack
-//	Allocate and initialize an execution stack.  The stack is
-//	initialized with an initial stack frame for ThreadRoot, which:
-//		enables interrupts
-//		calls (*func)(arg)
-//		calls NachOSThread::FinishThread
+//  Allocate and initialize an execution stack.  The stack is
+//  initialized with an initial stack frame for ThreadRoot, which:
+//      enables interrupts
+//      calls (*func)(arg)
+//      calls NachOSThread::FinishThread
 //
-//	"func" is the procedure to be forked
-//	"arg" is the parameter to be passed to the procedure
+//  "func" is the procedure to be forked
+//  "arg" is the parameter to be passed to the procedure
 //----------------------------------------------------------------------
 
 void
@@ -256,7 +256,7 @@ NachOSThread::AllocateThreadStack (VoidFunctionPtr func, int arg)
 
 #ifdef HOST_SNAKE
     // HP stack works from low addresses to high addresses
-    stackTop = stack + 16;	// HP requires 64-byte frame marker
+    stackTop = stack + 16;  // HP requires 64-byte frame marker
     stack[StackSize - 1] = STACK_FENCEPOST;
 #else
     // i386 & MIPS & SPARC stack works from high addresses to low addresses
@@ -264,7 +264,7 @@ NachOSThread::AllocateThreadStack (VoidFunctionPtr func, int arg)
     // SPARC stack must contains at least 1 activation record to start with.
     stackTop = stack + StackSize - 96;
 #else  // HOST_MIPS  || HOST_i386
-    stackTop = stack + StackSize - 4;	// -4 to be on the safe side!
+    stackTop = stack + StackSize - 4;   // -4 to be on the safe side!
 #ifdef HOST_i386
     // the 80386 passes the return address on the stack.  In order for
     // SWITCH() to go to ThreadRoot when we switch to this thread, the
@@ -275,7 +275,7 @@ NachOSThread::AllocateThreadStack (VoidFunctionPtr func, int arg)
 #endif  // HOST_SPARC
     *stack = STACK_FENCEPOST;
 #endif  // HOST_SNAKE
-    
+
     machineState[PCState] = (int) _ThreadRoot;
     machineState[StartupPCState] = (int) InterruptEnable;
     machineState[InitialPCState] = (int) func;
@@ -288,33 +288,33 @@ NachOSThread::AllocateThreadStack (VoidFunctionPtr func, int arg)
 
 //----------------------------------------------------------------------
 // NachOSThread::SaveUserState
-//	Save the CPU state of a user program on a context switch.
+//  Save the CPU state of a user program on a context switch.
 //
-//	Note that a user program thread has *two* sets of CPU registers -- 
-//	one for its state while executing user code, one for its state 
-//	while executing kernel code.  This routine saves the former.
+//  Note that a user program thread has *two* sets of CPU registers --
+//  one for its state while executing user code, one for its state
+//  while executing kernel code.  This routine saves the former.
 //----------------------------------------------------------------------
 
 void
 NachOSThread::SaveUserState()
 {
     for (int i = 0; i < NumTotalRegs; i++)
-	userRegisters[i] = machine->ReadRegister(i);
+    userRegisters[i] = machine->ReadRegister(i);
 }
 
 //----------------------------------------------------------------------
 // NachOSThread::RestoreUserState
-//	Restore the CPU state of a user program on a context switch.
+//  Restore the CPU state of a user program on a context switch.
 //
-//	Note that a user program thread has *two* sets of CPU registers -- 
-//	one for its state while executing user code, one for its state 
-//	while executing kernel code.  This routine restores the former.
+//  Note that a user program thread has *two* sets of CPU registers --
+//  one for its state while executing user code, one for its state
+//  while executing kernel code.  This routine restores the former.
 //----------------------------------------------------------------------
 
 void
 NachOSThread::RestoreUserState()
 {
     for (int i = 0; i < NumTotalRegs; i++)
-	machine->WriteRegister(i, userRegisters[i]);
+    machine->WriteRegister(i, userRegisters[i]);
 }
 #endif
