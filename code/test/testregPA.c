efc11599d2fc519b0e4f80995d6036ce5b37a5cb
Saksham Sharma <saksham0808@gmail.com>
2016-09-02 14:55:09 +0530

    Add SYScall_Time with tests

--- code/test/testregPA.c
+++ code/test/testregPA.c
@@ -15,8 +15,14 @@ main()
     system_call_PrintInt(system_call_GetPA(system_call_GetReg(29)));
     system_call_PrintChar('\n');
     for (i=0; i<SIZE; i++) array[i] = i;
+    system_call_PrintString("Time is: ");
+    system_call_PrintInt(system_call_GetTime());
+    system_call_PrintChar('\n');
     system_call_PrintString("We are currently at PC: ");
     system_call_PrintIntHex(system_call_GetReg(34));
+    system_call_PrintChar('\n');
+    system_call_PrintString("Time is: ");
+    system_call_PrintInt(system_call_GetTime());
     system_call_PrintChar('\n');
     for (i=0; i<SIZE; i++) sum += array[i];
     system_call_PrintString("Total sum: ");
--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -249,6 +249,13 @@ ExceptionHandler(ExceptionType which)
         machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
         machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
         machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    } else if ((which == SyscallException) && (type == SYScall_Time)) {
+        machine->WriteRegister(2, stats->totalTicks);
+
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     } else {
         printf("Unexpected user mode exception %d %d\n", which, type);
         ASSERT(FALSE);
