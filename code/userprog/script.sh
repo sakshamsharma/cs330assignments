7317068175e2653c7fa4f2f58c1ed8665d409264
Saksham Sharma <saksham0808@gmail.com>
2016-10-28 15:00:54 +0530

    Add batches

---
+++ code/userprog/script.sh
@@ -0,0 +1,14 @@
+for index in {1..4};
+do
+    echo "File Batch${index}"
+
+    for i in {1..10};
+    do
+        echo -n "|${i}"
+        sed -i -E "1s/[0-9]+/${i}/g" Batch${index}
+        ./nachos -F Batch${index} | tail -n 14 | head -n 12 | cut -f 2 -d ':' -s | awk 'BEGIN {printf "|"}; {printf "%s|", $1}; END {print ""}'
+    done;
+
+    echo ""
+    echo ""
+done
